98ba193e520a100c25312be24b83dcd46b70869f
jonathan parisot <parisot.jonathan@gmail.com>
2017-09-23 16:33:02 +0200

    added the comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923142137) do
+ActiveRecord::Schema.define(version: 20170923143139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "contact_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_comments_on_contact_id", using: :btree
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.integer  "user_id"
@@ -43,5 +51,6 @@ ActiveRecord::Schema.define(version: 20170923142137) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "contacts"
   add_foreign_key "contacts", "users"
 end
